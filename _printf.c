628ca4be55ae0a8ee20acc7087c1dabeb6b8833a
josephlaryeanortey <christiangospelhype@gmail.com>
2023-10-17 19:34:42 -0700

    task0

--- _printf.c
+++ _printf.c
@@ -1,66 +1,62 @@
-#include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - Custom printf function
+ * @format: Format string
+ *
+ * Return: Number of characters printed (excluding the null byte)
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+    va_list args;
+    int count = 0;
+    const char *ptr;
+    char c;
+    char *str;
 
-	if (format == NULL)
-		return (-1);
+    va_start(args, format);
 
-	va_start(list, format);
+    for (ptr = format; *ptr != '\0'; ptr++)
+    {
+        if (*ptr == '%')
+        {
+            ptr++; /* Move to the character after '%' */
+            switch (*ptr)
+            {
+            case 'c':
+                c = (char) va_arg(args, int);
+                write(1, &c, 1);
+                count++;
+                break;
+            case 's':
+                str = va_arg(args, char *);
+                while (*str != '\0')
+                {
+                    write(1, str, 1);
+                    str++;
+                    count++;
+                }
+                break;
+            case '%':
+                write(1, "%", 1);
+                count++;
+                break;
+            default:
+                /* Ignore unsupported format specifiers */
+                break;
+            }
+        }
+        else
+        {
+            write(1, ptr, 1);
+            count++;
+        }
+    }
 
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
+    va_end(args);
 
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
+    return count;
 }
 
-/**
- * print_buffer - Prints the contents of the buffer if it exists
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, buffer, *buff_ind);
 
-	*buff_ind = 0;
-}
--- main.h
+++ main.h
@@ -1,11 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
-#include <unistd.h>
 
-int _putchar(char c);
-int print_string(char *str);
-int print_number(int n);
+#include <stdarg.h>
+
 int _printf(const char *format, ...);
 
-#endif
+#endif /* MAIN_H */
