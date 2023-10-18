94498b85b13ca520e8441da4c996f260f5ba72c5
Racem BOUCHNAK <racem.bouchn@gmail.com>
2023-10-17 19:35:41 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,45 +1,51 @@
 #include "main.h"
+
 /**
-*_printf - entry point of the code
-*@format:input
-* Return:length of string
+* _printf - prints anything
+* @format: the format string
+*
+* Return: number of bytes printed
 */
 int _printf(const char *format, ...)
 {
-if (format)
-{
-unsigned int i, s_len, r = 0;
-va_list x;
-if (format == NULL)
-return (1);
-va_start(x, format);
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] != '%')
-{
-write(1, &format[i], 1);
-r++;
-}
-else if (format[i + 1] == 'c')
-{
-_putchar(va_arg(x, int));
-i++;
-}
-else if (format[i + 1] == '%')
-{
-_putchar('%');
-i++;
-}
-else if (format[i + 1] == 's')
-{
-s_len = print_string(va_arg(x, char*));
-r += s_len;
-i++;
-}
-}
+int sum = 0;
+va_list ap;
+char *p, *start;
+params_t params = PARAMS_INIT;
 
-va_end(x);
-return (r);
+va_start(ap, format);
+
+if (!format || (format[0] == '%' && !format[1]))
+return (-1);
+
+if (format[0] == '%' && format[1] == ' ' && !format[2])
+return (-1);
+
+for (p = (char *)format; *p; p++)
+{
+init_params(&params, ap);
+if (*p != '%')
+{
+sum += _putchar(*p);
+continue;
 }
-return (1);
+start = p;
+p++;
+while (get_flag(p, &params))
+{
+p++;
+}
+p = get_width(p, &params, ap);
+p = get_precision(p, &params, ap);
+if (get_modifier(p, &params))
+p++;
+if (!get_specifier(p))
+sum += print_from_to(start, p, params.l_modifier ||
+params.h_modifier ? p - 1 : 0);
+else
+sum = get_print_func(p, ap, &params);
+}
+_putchar(BUF_FLUSH);
+va_end(ap);
+return (sum);
 }
